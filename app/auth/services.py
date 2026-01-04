7231cce20f3578c69b7d5088df389eee0a7784f5
adeelism <adeelism12@gmail.com>
2026-01-04 17:37:26 +0500

    fixed minor issue in status code

--- app/auth/services.py
+++ app/auth/services.py
@@ -61,11 +61,10 @@ def create_access_token(
 
 def authenticate_user(*, request: LoginRequest, db: Session) -> User:
     user: Optional[User] = db.query(User).filter(User.email == request.email).first()
-    hashed_pw: str = cast(str, user.hashed_password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_NOT_FOUND, detail="Invalid Credentials")
-    if not verify_password(request.password, hashed_pw):
-        raise HTTPException(status_code=status.HTTP_401_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Email")
+    if not verify_password(request.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Password")
     if not user.is_active:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Inactive user")
     return user
